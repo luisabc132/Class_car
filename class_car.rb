7eb0eccaf920322d64d570d92ee6fc3a5a0b4a81
Luis Briceño <luisabc132@gmail.com>
2016-10-12 11:51:07 +0200

    Add Couter for car

--- class_car.rb
+++ class_car.rb
@@ -1,12 +1,15 @@
 class Car
-
-
+@@contador_coches = 0
 def initialize (sound)
   @sound = sound
+  @@contador_coches += 1
 end
 
 def make_sound
   puts @sound
+end
+def contador
+  @@contador_coches
 end
 
 end
@@ -14,5 +17,9 @@ end
 car1= Car.new("brum!")
 car2= Car.new("brum!brum!brum!brum!")
 car3= Car.new("run..run..run..run..run..run" )
+car4= Car.new("brum!")
+car5= Car.new("brum!brum!brum!brum!")
+car6= Car.new("run..run..run..run..run..run" )
 puts car2.make_sound
 puts car3.make_sound
+puts car3.contador
